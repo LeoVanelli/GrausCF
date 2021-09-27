237eef9cf59eab5df20c6581271886e6588dc997
Leandro Gonçalves Vanelli <55560279+LeoVanelli@users.noreply.github.com>
2021-09-26 23:39:55 -0300

    Adiciona atividade GrausCF

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GrausCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Objetivo: Converta uma temperatura digitada pelo usuário em °C para °F.
+
+            Console.Write("Digite o valor da temperatura em ºC: ");
+            int C = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nResposta: {C}ºC equivalem à {C*1.8+32}ºF");
+        }
+    }
+}
